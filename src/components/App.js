cf9019f29b7056c8ed2d6ccfd3388b71a2704cb4
spences10 <spences10apps@gmail.com>
2017-03-22 08:32:43 +0000

    Renamed wrongly named components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Header'
+import ParallaxHeader from './ParallaxHeader'
 import Contact from './Contact'
 
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
     return (
       <div>
         <Header/>
+        <ParallaxHeader/>
         <Contact/>
 			</div>
     )
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-class Header extends React.Component {
+class Contact extends React.Component {
 	
 	render () {
 		return (
-			<div id='contact' className='contact'>
+			<div className='contact'>
         <div className=''>
           Voluptate quis exercitation cillum ea cillum enim.
           Cupidatat aliqua mollit ad pariatur irure excepteur exercitation non. Ullamco occaecat velit duis duis mollit eiusmod irure laboris cillum qui duis amet. Laboris et duis est labore reprehenderit. Consectetur reprehenderit aliqua ut nostrud ex ea velit laborum consequat. Irure qui cupidatat nostrud excepteur aute cillum commodo. Ea magna non consectetur labore ad occaecat irure amet.
@@ -14,4 +14,4 @@ class Header extends React.Component {
 	}
 }
 
-export default Header;
+export default Contact;
