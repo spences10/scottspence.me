d9f827892e94a899a1435fc98a746b0dff2fd238
spences10 <spences10apps@gmail.com>
2017-03-23 17:34:37 +0000

    add about and portfolio components

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        Id in labore cupidatat deserunt ipsum velit anim id.
+      </div>
+    )
+  }
+}
+
+export default About
