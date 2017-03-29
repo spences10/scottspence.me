eb932b438dd1df9ba735404a7f247adbb5ebb58e
spences10 <spences10apps@gmail.com>
2017-03-29 21:12:40 +0100

    add detail

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { FaFire } from 'react-icons/lib/fa'
 
 class About extends React.Component {
   render() {
     return (
       <div id="about" className="about">
         <h1>About</h1>
-        <h2>Id in labore cupidatat deserunt ipsum velit anim id.</h2>
+        <div className="about-start">
+          Here you can view the various projects I have made using a range of frameworks and libraries whilst continuing my full stack developer certification with freeCodeCamp(<FaFire />)
+        </div>
+        <div className="">
+          This page was made with React and Stylus 
+        </div>
       </div>
     )
   }
