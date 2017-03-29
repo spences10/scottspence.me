93a4918139c2ec6678e5162a5f07d3e5b8981086
spences10 <spences10apps@gmail.com>
2017-03-29 11:22:10 +0100

    pass handleScroll to Nav

--- src/components/App.js
+++ src/components/App.js
@@ -7,28 +7,31 @@ import Portfolio from './Portfolio'
 import Contact from './Contact'
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.ref = 'parallax'
+  constructor(props) {
+    super(props)
+    this.handleScroll = value => this.parallax && this.parallax.scrollTo(value)
   }
   render() {
     return (
       <div>
-        <Parallax ref={this.ref} pages={4}>
+        <Parallax
+          ref={ref => (this.parallax = ref)}
+          pages={4}
+        >
 
           <Parallax.Layer
             offset={0}
             speed={0.5}
-            onClick={() => this.refs.parallax.scrollTo(1)}
+            onClick={() => this.parallax.scrollTo(1)}
           >
-            <Nav />
+            <Nav handleScroll={this.handleScroll} />
             <Header />
           </Parallax.Layer>
 
           <Parallax.Layer
             offset={1}
             speed={0.5}
-            onClick={() => this.refs.parallax.scrollTo(2)}
+            onClick={() => this.parallax.scrollTo(2)}
           >
             <About />
           </Parallax.Layer>
@@ -36,7 +39,7 @@ class App extends React.Component {
           <Parallax.Layer
             offset={2}
             speed={0.5}
-            onClick={() => this.refs.parallax.scrollTo(3)}
+            onClick={() => this.parallax.scrollTo(3)}
           >
             <Portfolio />
           </Parallax.Layer>
@@ -44,7 +47,7 @@ class App extends React.Component {
           <Parallax.Layer
             offset={3}
             speed={0.5}
-            onClick={() => this.refs.parallax.scrollTo(0)}
+            onClick={() => this.parallax.scrollTo(0)}
           >
             <Contact />
           </Parallax.Layer>
