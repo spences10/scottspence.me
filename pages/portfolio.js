058b8b264cc124d5c5665bc3c7ab8125fb468123
spences10 <spences10apps@gmail.com>
2017-05-27 11:39:13 +0100

    Map over portfolio items :+1:

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,9 +2,28 @@ import React from 'react'
 import Layout from '../components/Layout.js'
 import Project from '../components/Project'
 
-
-export default () => (
+const Index = (props) => (
   <Layout>
     <p>Portfolio</p>
+    <div>
+      {props.projects.map(project => <Project key={project.id} {...project} />)}
+    </div>
   </Layout>
 )
+
+Index.getInitialProps = function () {
+  const projects = [
+    { id: 1, name: 'Project 1', desc: 'Project description: Project 1', github: '', preview: '', image: '' },
+    { id: 2, name: 'Project 2', desc: 'Project description: Project 2', github: '', preview: '', image: '' },
+    { id: 3, name: 'Project 3', desc: 'Project description: Project 3', github: '', preview: '', image: '' },
+    { id: 4, name: 'Project 4', desc: 'Project description: Project 4', github: '', preview: '', image: '' }
+  ]
+
+  console.log(`Show data fetched. Count: ${projects.length}`)
+
+  return {
+    projects
+  }
+}
+
+export default Index
