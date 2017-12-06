834c797f3574949d6bb57fb2d3057a85b809c72e
spences10 <spences10apps@gmail.com>
2017-12-06 06:39:43 +0000

    use faker

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,21 +1,48 @@
 import React from 'react'
 import { Project } from './Project'
+import faker from 'faker'
 
 this.state = {
   projects: [
-    { id: 1, name: 'Project 1', desc: 'Project description: Project 1', github: '', preview: '', image: '' },
-    { id: 2, name: 'Project 2', desc: 'Project description: Project 2', github: '', preview: '', image: '' },
-    { id: 3, name: 'Project 3', desc: 'Project description: Project 3', github: '', preview: '', image: '' },
-    { id: 4, name: 'Project 4', desc: 'Project description: Project 4', github: '', preview: '', image: '' }
+    {
+      id: 1,
+      name: faker.commerce.productName(),
+      desc: faker.company.catchPhraseDescriptor(),
+      github: faker.internet.url(),
+      image: faker.image.imageUrl()
+    },
+    {
+      id: 2,
+      name: faker.commerce.productName(),
+      desc: faker.company.catchPhraseDescriptor(),
+      github: faker.internet.url(),
+      image: faker.image.imageUrl()
+    },
+    {
+      id: 3,
+      name: faker.commerce.productName(),
+      desc: faker.company.catchPhraseDescriptor(),
+      github: faker.internet.url(),
+      image: faker.image.imageUrl()
+    },
+    {
+      id: 4,
+      name: faker.commerce.productName(),
+      desc: faker.company.catchPhraseDescriptor(),
+      github: faker.internet.url(),
+      image: faker.image.imageUrl()
+    }
   ]
 }
 
 export const Portfolio = () => (
   <div id="portfolio" className="portfolio">
     <h1>Portfolio</h1>
-    <p>Et do culpa mollit ea aliqua ipsum.</p>
+    <p>List of projects here:</p>
     <div className="projects-container">
-      {this.state.projects.map(project => <Project key={project.id} {...project} />)}
+      {this.state.projects.map(project => (
+        <Project key={project.id} {...project} />
+      ))}
     </div>
   </div>
 )
