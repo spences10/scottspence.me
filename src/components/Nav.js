20bf8694d7e648a5da3dc427457d23f1c9d7b9aa
spences10 <spences10apps@gmail.com>
2017-12-07 13:50:20 +0000

    move some shit get rid of styles, bc styled-components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import logo from '../styles/images/logo.svg'
+import logo from '../images/logo.svg'
 
 const StyledLogo = styled.img`
   height: 30px;
