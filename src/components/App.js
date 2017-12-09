f776c3135799db68dc6e26924d1f74910b1ac12e
spences10 <spences10apps@gmail.com>
2017-12-09 19:16:51 +0000

    injectGlobal

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,42 @@
 import React from 'react'
+import { injectGlobal } from 'styled-components'
 
 import Nav from './Nav'
 import Main from './Main'
+
+injectGlobal`
+  html {
+    color: #292f33;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    line-height: 1.3125;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    margin: 0 auto;
+  }
+
+  svg {
+    fill: currentColor;
+    height: 1.25em;
+  }
+
+  @media screen and (min-width: 360px) {
+    html {
+      font-size: 15px;
+    }
+  }
+
+  @media screen and (min-width: 600px) {
+    html {
+      font-size: 16px;
+    }
+  }
+`
 
 const App = () => (
   <div>
