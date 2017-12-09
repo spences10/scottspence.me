67d0cb19e79d3d7e8bc39567ce88929be963c282
spences10 <spences10apps@gmail.com>
2017-12-09 19:16:16 +0000

    add image

--- src/components/Hi.js
+++ src/components/Hi.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Grid } from 'grid-styled'
+import styled from 'styled-components'
+
 import Person from './Person'
+
+const Image = styled.img`
+  width: 100%;
+`
 
 export const Hi = () => (
   <div>
     <h1>Hi</h1>
-    <Person />
+    <Image src={require('../images/city_skyline.jpeg')} alt="city_skyline" />
+    <Grid p={2} width={[1, 1 / 2, 1 / 4]}>
+      <Person />
+    </Grid>
   </div>
 )
 
