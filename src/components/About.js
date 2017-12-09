1d2f5878ff8b20aabdd36751436228e686953354
spences10 <spences10apps@gmail.com>
2017-12-09 19:16:34 +0000

    remove className

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import { FaFire } from 'react-icons/lib/fa'
 import styled from 'styled-components'
 
-const Body = styled.body`
+const Body = styled.div`
   height: 100vh;
   background: pink;
   border 2px solid pink;
@@ -17,17 +17,15 @@ const StyledH1 = styled.h1`
 class About extends React.Component {
   render() {
     return (
-      <div id="about" className="about">
-        <Body>
-          <StyledH1>About</StyledH1>
-          <div className="about-start">
-            Here you can view the various projects I have made using a range of
-            frameworks and libraries whilst continuing my full stack developer
-            certification with freeCodeCamp()
-          </div>
-          <div className="">This page was made with React</div>
-        </Body>
-      </div>
+      <Body>
+        <StyledH1>About</StyledH1>
+        <div className="about-start">
+          Here you can view the various projects I have made using a range of
+          frameworks and libraries whilst continuing my full stack developer
+          certification with freeCodeCamp()
+        </div>
+        <div className="">This page was made with React</div>
+      </Body>
     )
   }
 }
