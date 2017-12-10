6f81bae1a77e47346cca4c54428817993096a997
spences10 <spences10apps@gmail.com>
2017-12-10 09:00:20 +0000

    remove unused code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,29 +34,4 @@ const Navbar = () => (
   </nav>
 )
 
-// class Nav extends React.Component {
-//   render() {
-//     return (
-//       <div className="nav">
-//         <ul className="links">
-//           <li>
-//             <a href="#home">
-//               <StyledLogo img src={logo} className="logo" alt="logo" />
-//             </a>
-//           </li>
-//           <li>
-//             <a href="#about">About</a>
-//           </li>
-//           <li>
-//             <a href="#portfolio">Portfolio</a>
-//           </li>
-//           <li>
-//             <a href="#contact">Contact</a>
-//           </li>
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
 export default Navbar
