5c64f7884d6ed9a5e6cb76fda83d91fbe626ab3c
spences10 <spences10apps@gmail.com>
2017-12-10 08:34:14 +0000

    moar global styles

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -3,7 +3,41 @@ import { injectGlobal } from 'styled-components'
 injectGlobal`
   @import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto|VT323');
 
+  /* body {
+    font-family: 'Roboto';
+    font-size: 20px
+  } */
+
   body {
-    font-family: 'Roboto'
+    color: purple;
+    font-family: Roboto, Arial, sans-serif;
+    font-size: 20px;
+    line-height: 1.3125;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    margin: 0 auto;
+    
+  }
+
+  /* svg {
+    fill: currentColor;
+    height: 1.25em;
+  } */
+
+  @media screen and (min-width: 360px) {
+    html {
+      font-size: 15px;
+    }
+  }
+
+  @media screen and (min-width: 600px) {
+    html {
+      font-size: 16px;
+    }
   }
 `
