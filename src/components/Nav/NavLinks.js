663166fc383b4d551cac1aafb1ff3d518334e3b3
spences10 <spences10apps@gmail.com>
2017-12-14 08:47:40 +0000

    use nav separator

--- src/components/Nav/NavLinks.js
+++ src/components/Nav/NavLinks.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+import NavSeparator from './NavSeparator'
+
 const Wrapper = styled.nav`
   display: flex;
   align-items: center;
@@ -11,8 +13,11 @@ const Wrapper = styled.nav`
 const NavLinks = () => (
   <Wrapper>
     <Link to="/">Home</Link>
+    <NavSeparator />
     <Link to="/about">About</Link>
+    <NavSeparator />
     <Link to="/portfolio">Portfolio</Link>
+    <NavSeparator />
     <Link to="/contact">Contact</Link>
   </Wrapper>
 )
--- src/components/Nav/NavSeparator.js
+++ src/components/Nav/NavSeparator.js
@@ -4,10 +4,9 @@ import rem from '../../utils/rem'
 
 const NavSeparator = styled.span`
   flex: 0 0 auto;
-  /* width: ${rem(5)}; */
-  /* height: ${rem(5)}; */
-  /* margin: 0 ${rem(15)}; */
-
+  width: ${rem(5)};
+  height: ${rem(5)};
+  margin: 0 ${rem(15)};
   border-radius: 50%;
   background: currentColor;
   opacity: 0.35;
