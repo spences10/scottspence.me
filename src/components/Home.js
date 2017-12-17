68a9a16b9b09557590f7cb3561569741b112db2a
spences10 <spences10apps@gmail.com>
2017-12-17 10:58:31 +0000

    Add person card

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,9 @@ import Person from './Person'
 
 import { StyledH1 } from '../theme/globalStyle'
 
+const img =
+  'https://pbs.twimg.com/profile_images/697901266945646592/05jKr58e_400x400.jpg'
+
 const Image = styled.img`
   width: 100%;
 `
@@ -15,7 +18,11 @@ export const Home = () => (
     <StyledH1>Hi</StyledH1>
     <Image src={require('../images/city_skyline.jpeg')} alt="city_skyline" />
     <Grid p={2} width={[1, 1 / 2, 1 / 4]}>
-      <Person />
+      <Person
+        title={'Scott Spence'}
+        text={'Aspiring full stack web developer'}
+        img={img}
+      />
     </Grid>
   </div>
 )
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,33 +1,62 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const img =
-  'https://pbs.twimg.com/profile_images/697901266945646592/05jKr58e_400x400.jpg'
+import { StyledH3, StyledP, ColorPalette as CP } from '../theme/globalStyle'
+import { ButtonSmall } from '../components/Button'
 
-const Card = styled.div`
-  padding: 1.75rem;
-  margin: 0.5rem;
+const Wrapper = styled.div`
+  width: 400px;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: auto;
+  grid-template-areas: 'img text text';
+  background: ${CP.primary.light};
   border-radius: 4px;
-  color: white;
-  background: purple;
 `
 
-const Image = styled.div`
-  padding: 1.75rem;
+const CardTitle = StyledH3.extend`
+  color: ${CP.white};
+  margin: 0.05rem;
+  padding: 0rem;
+`
+
+const CardText = StyledP.extend`
+  color: ${CP.white};
+  margin: 0.05rem;
+  padding: 0rem;
+`
+
+const Text = styled.div`
+  grid-area: text;
+  padding: 0.5rem;
   margin: 0.5rem;
+`
+
+const Image = styled.img`
+  grid-area: img;
+  justify-self: center;
+  margin: 1.3rem 0.5rem 0.5rem 0.5rem;
   border-radius: 90px;
   border: 1px solid #000;
-  background-image: url(${img});
-  background-size: 100px;
-  width: 40px;
-  height: 40px;
+  width: 6.25rem;
+  height: 6.25rem;
 `
 
-export const Person = () => (
-  <Card>
-    <h1>This is the person</h1>
-    <Image />
-  </Card>
-)
+class Person extends React.Component {
+  render() {
+    return (
+      <Wrapper>
+        <Image src={this.props.img} />
+        <Text>
+          <CardTitle>{this.props.title}</CardTitle>
+          <CardText>{this.props.text}</CardText>
+          <ButtonSmall color={CP.white} border={CP.white}>
+            Check Me Out!
+          </ButtonSmall>
+        </Text>
+      </Wrapper>
+    )
+  }
+}
 
 export default Person
