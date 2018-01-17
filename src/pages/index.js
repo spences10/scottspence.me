7356fb9bd4efa299bf774ae0eea285ad83c2f9f0
spences10 <spences10apps@gmail.com>
2018-01-17 19:01:36 +0000

    renam page 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hi people 👋</h1>
     <p>Welcome to my new Gatsby portfolio site.</p>
     <p>watch this space whilst I develop this. 👍</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/about/">Go to page 2</Link>
   </div>
 )
 
