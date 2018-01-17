263cd1d35a20b854f5fe48923f2023a4ff765330
spences10 <spences10apps@gmail.com>
2018-01-17 19:01:03 +0000

    add about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const SecondPage = () => (
+const AboutPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Hi from the about page</h1>
+    <p>Currently page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
 
-export default SecondPage
+export default AboutPage
