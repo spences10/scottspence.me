e7ac52c6394df873617b5754f90455f0a57aafb6
spences10 <spences10apps@gmail.com>
2018-01-17 18:54:50 +0000

    add basics

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi people 👋</h1>
+    <p>Welcome to my new Gatsby portfolio site.</p>
+    <p>watch this space whilst I develop this. 👍</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
