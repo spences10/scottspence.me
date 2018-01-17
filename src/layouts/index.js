32dd45e5b00b38bb1a29e98281abbf6af69d9569
spences10 <spences10apps@gmail.com>
2018-01-17 15:28:10 +0000

    add theme provider

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled, { ThemeProvider } from 'styled-components'
 
 import Header from '../components/Header'
+import { theme } from '../theme/globalStyle'
+
 import './index.css'
 
+const PageContainer = styled.div``
+
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
+  <ThemeProvider theme={theme}>
+    <PageContainer>
+      <Helmet
+        title="Scott Spence - portfolio"
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' }
+        ]}
+      />
+      <Header />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0
+        }}>
+        {children()}
+      </div>
+    </PageContainer>
+  </ThemeProvider>
 )
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+  children: PropTypes.func
 }
 
 export default TemplateWrapper
