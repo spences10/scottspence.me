0bbc8433902b8e740d81ed237aa9a89b847f96ee
spences10 <spences10apps@gmail.com>
2018-01-17 15:27:47 +0000

    add globalStyle

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,24 @@
+import { injectGlobal } from 'styled-components'
+
+export const theme = {
+  text: '#333',
+  white: '#fff',
+  primary: {
+    dark: '#453463',
+    light: '#755f9f'
+  },
+  secondary: {
+    green: '#8ac3a9',
+    yellow: '#fcdeb6',
+    red: '#ff8463'
+  },
+  shades: {
+    dark: 'rgba(69, 52, 99, 0.5)',
+    offWhite: 'rgb(244, 254, 254)'
+  }
+}
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto|VT323');
+
+`
