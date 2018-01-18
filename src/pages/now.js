a8740f9b5eccaae5f1502c85d1b71ed508e13629
spences10 <spences10apps@gmail.com>
2018-01-18 18:10:02 +0000

    add now and uses pages

---
+++ src/pages/now.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const NowPage = () => (
+  <div>
+    now things
+    <Link to="/">Go home</Link>
+  </div>
+)
+
+export default NowPage
