21d735e6cf4fd08fc40778833d4e21c027c87135
spences10 <spences10apps@gmail.com>
2018-01-18 17:11:26 +0000

    imporve meta data remove starter styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -14,20 +14,25 @@ const TemplateWrapper = ({ children, data }) => (
       <Helmet
         title="Scott Spence - portfolio"
         meta={[
-          { name: 'description', content: 'Sample' },
-          { name: 'keywords', content: 'sample, something' }
+          {
+            name: 'description',
+            content:
+              'Portfolio site of Scott Spence - aspiring web developer'
+          },
+          {
+            name: 'keywords',
+            content: 'portfolio, web developer, javascript'
+          },
+          {
+            name: 'image',
+            content:
+              'https://pbs.twimg.com/profile_images/697901266945646592/05jKr58e_400x400.jpg'
+          }
         ]}
       />
       <Header navItems={data.site.siteMetadata.pages} />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0
-        }}>
-        {children()}
-      </div>
+
+      <div>{children()}</div>
     </PageContainer>
   </ThemeProvider>
 )
