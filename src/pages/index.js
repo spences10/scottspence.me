053f32cebdd82fead48ae2253122a27e772a3115
spences10 <spences10apps@gmail.com>
2018-01-21 01:51:04 +0000

    span thing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,18 @@ import { media } from '../theme/globalStyle'
 
 import { PageContainer } from '../theme/globalStyle'
 
+const Span = styled.div`
+  grid-column: 1 / span 9;
+`
+
 const IndexPage = () => (
   <PageContainer>
-    <div>
+    <Span>
       <h1>Hi people 👋</h1>
       <p>Welcome to my new Gatsby portfolio site.</p>
       <p>watch this space whilst I develop this. 👍</p>
       <Link to="/about/">Go to about page</Link>
-    </div>
+    </Span>
   </PageContainer>
 )
 
