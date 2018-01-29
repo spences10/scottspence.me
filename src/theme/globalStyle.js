bf56a2648963ee7d78b7b86cdfebeaf3aa3353f0
spences10 <spences10apps@gmail.com>
2018-01-29 15:55:16 +0000

    try Titillium Web

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -33,7 +33,7 @@ export const theme = {
 }
 
 injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto|VT323');
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto|Titillium+Web');
 
   *, *:before, *:after {
     box-sizing: border-box;
