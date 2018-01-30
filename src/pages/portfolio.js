a6570f4296b87da7483b5dc077ff30bfafc80328
spences10 <spences10apps@gmail.com>
2018-01-30 16:14:48 +0000

    move Protfolio component to portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,8 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
-import Portfolio from '../components/Portfolio'
+import { Project } from '../components/Project'
+import styled from 'styled-components'
 import Img from 'gatsby-image'
+
+import { media } from '../theme/globalStyle'
+
+import { projectsApi } from '../utils/api'
+
+const ProjectWrapper = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(3, 1fr);
+
+  ${media.giant`
+    grid-template-columns: repeat(2, 1fr);
+  `};
+  ${media.desktop`
+    grid-template-columns: repeat(2, 1fr);
+  `};
+  ${media.tablet`
+    grid-template-columns: repeat(1, 1fr);
+  `};
+  ${media.phone`
+    grid-template-columns: repeat(1, 1fr);
+  `};
+`
 
 const PortfolioPage = ({ data }) => {
   const { edges: imgData } = data.allFile
@@ -15,27 +39,22 @@ const PortfolioPage = ({ data }) => {
     <div>
       <p>portfolio things</p>
 
-      {imgData.map((property, index) => (
-        <Img
-          key={index}
-          alt={property.node.relativePath}
-          resolutions={property.node.childImageSharp.resolutions}
-        />
-      ))}
-      {/* <Img
-        alt={'props.name'}
-        resolutions={property.node.resolutions}
-      /> */}
-      {/* <Img
-        alt={'props.name'}
-        resolutions={data.project2.childImageSharp.resolutions}
-      /> */}
-      {/* <Img
-        alt={'props.name'}
-        resolutions={data.project3.childImageSharp.resolutions}
-      /> */}
+      <h1>Portfolio</h1>
+      <p>List of projects here:</p>
+      {/* {this.props.imgData.map((property, index) => (
+          <Img
+            key={index}
+            alt={property.node.relativePath}
+            resolutions={property.node.childImageSharp.resolutions}
+          />
+        ))} */}
 
-      <Portfolio imgData={imgData} />
+      <ProjectWrapper>
+        {projectsApi.projects.map(project => (
+          <Project key={project.id} {...project} />
+        ))}
+      </ProjectWrapper>
+
       <Link to="/">Go home</Link>
     </div>
   )
@@ -55,28 +74,6 @@ export const query = graphql`
         }
       }
     }
-
-    # project1: file(relativePath: { regex: "/project1/" }) {
-    #   childImageSharp {
-    #     resolutions(width: 125, height: 125) {
-    #       ...GatsbyImageSharpResolutions
-    #     }
-    #   }
-    # }
-    # project2: file(relativePath: { regex: "/project2/" }) {
-    #   childImageSharp {
-    #     resolutions(width: 125, height: 125) {
-    #       ...GatsbyImageSharpResolutions
-    #     }
-    #   }
-    # }
-    # project3: file(relativePath: { regex: "/project3/" }) {
-    #   childImageSharp {
-    #     resolutions(width: 125, height: 125) {
-    #       ...GatsbyImageSharpResolutions
-    #     }
-    #   }
-    # }
   }
 `
 
