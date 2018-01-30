40eb7bba0d68cad78b824526d716b71f072ea20a
spences10 <spences10apps@gmail.com>
2018-01-30 16:15:04 +0000

    move projects out to fake api

---
+++ src/utils/api.js
@@ -0,0 +1,52 @@
+export const projectsApi = {
+  projects: [
+    {
+      id: 1,
+      name: 'Project 1',
+      desc: 'One line project 1 description',
+      github: 'https://someproject1.com',
+      demo: 'https://someprojectdemo1.com',
+      image: 'project1.jpeg'
+    },
+    {
+      id: 2000,
+      name: 'Project 2',
+      desc: 'One line project 2 description',
+      github: 'https://someproject2.com',
+      demo: 'https://someprojectdemo2.com',
+      image: 'project2.jpeg'
+    },
+    {
+      id: 3,
+      name: 'Project 3',
+      desc: 'One line project 3 description',
+      github: 'https://someproject3.com',
+      demo: 'https://someprojectdemo3.com',
+      image: 'project3.jpeg'
+    },
+    {
+      id: 4,
+      name: 'Project 4',
+      desc: 'One line project 4 description',
+      github: 'https://someproject4.com',
+      demo: 'https://someprojectdemo4.com',
+      image: 'project4.jpeg'
+    },
+    {
+      id: 5,
+      name: 'Project 5',
+      desc: 'One line project 5 description',
+      github: 'https://someproject5.com',
+      demo: 'https://someprojectdemo5.com',
+      image: 'project5.jpeg'
+    },
+    {
+      id: 6,
+      name: 'Project 6',
+      desc: 'One line project 6 description',
+      github: 'https://someproject6.com',
+      demo: 'https://someprojectdemo6.com',
+      image: 'project6.jpeg'
+    }
+  ]
+}
