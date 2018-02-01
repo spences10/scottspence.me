b1bb95925919bc931be7e2be6f4fee100224e777
spences10 <spences10apps@gmail.com>
2018-02-01 17:02:34 +0000

    add contact page

--- src/components/PageNav.js
+++ src/components/PageNav.js
@@ -8,7 +8,7 @@ import { media } from '../theme/globalStyle'
 const NavMenu = styled.ul`
   grid-area: n;
   display: inline-grid;
-  grid-template-columns: repeat(4, auto);
+  grid-template-columns: repeat(5, auto);
   grid-column-gap: 10px;
   margin: 1.5rem 0.1rem 1.5rem 0.1rem;
   padding: 1.5rem 0.1rem 1.5rem 0.1rem;
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const ContactPage = () => (
+  <div>
+    <h1>Get in touch</h1>
+    <p>
+      <Link to="/">Go home</Link>
+    </p>
+  </div>
+)
+
+export default ContactPage
