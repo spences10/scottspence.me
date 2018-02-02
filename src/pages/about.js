bea23cfeea019209ae4bce2b83bf4c3e1ddc0dad
spences10 <spences10apps@gmail.com>
2018-02-02 15:17:15 +0000

    add about details

--- src/pages/about.js
+++ src/pages/about.js
@@ -33,11 +33,92 @@ const StyledLink = styled.a`
   }
 `
 
+const StyledLi = styled.li`
+  margin: 1rem;
+`
+
 const AboutPage = () => (
   <PageWrapper>
-    <h1>Hi from the about page</h1>
+    <h1>Hello from London</h1>
+    <p>Hi I&#39;m Scott Spence 👋</p>
+    <h3>Backstory</h3>
+    <p>
+      I&#39;m an analyst developer in the financial sector working in
+      the City of London, I&#39;m marriwed with a beautiful wife and
+      daughter. At 41 I have decided that [as there are not be as many
+      VBA developer jobs on the market these days] I should re-skill
+      so that I am still employable in ten more years time.
+    </p>
+    <h3>Coding history</h3>
+    <p>
+      I learned to code [VBA] whilst working my first &#39;proper&#39;
+      job at Barclays Bank as an assistant to the finance manager of a
+      business area, the scripting was to automate credit card
+      reconciliation from SAP and for producing monthly reporting
+      figures, I was lucky enough to have been spotted by one of the
+      development managers on a different team and offered a job as an
+      Analyst Developer back in 2007, for the past 10 years I have
+      mainly worked with VBA in Excel, Access, Word and in some cases
+      PowerPoint!
+    </p>
+    <p>
+      The majority of solutions I made at Barclays were workflow
+      tools, the tracking of a cheque or payment through a process,
+      lots of greenfeild work with the focus on using the tools the
+      business had to hand, so, no SQL servers, just the MS Office
+      suite, &#34;user needs a database? Ok cool lets set up an MS
+      Access db on a file server somewhere&#34; yes!
+    </p>
+    <p>
+      Sectors I have worked in: Finance [Risk, Asset Management],
+      Property, Oil &#38; Gas and currently in the Lloyd&#39;s
+      insurance market.
+    </p>
+    <p>Tools of my trade: VBA, SQL some VB.NET and C#</p>
 
-    <p>Hi I&#39;m Scott,</p>
+    <h3>My interests</h3>
+    <p>
+      As I&#39;m devoting nearly every spare hour I have to sitting in
+      front of a screen and trying to make something happen, my main
+      interests are currently:
+    </p>
+
+    <StyledLi>JavaScript learning all teh things!</StyledLi>
+
+    <StyledLi>
+      React - check the <Link to="/portfolio">portfolio page</Link>{' '}
+      for details
+    </StyledLi>
+
+    <StyledLi>Node - made several Twitter bots.</StyledLi>
+
+    <StyledLi>GraphQL</StyledLi>
+
+    <p>Things on my &#39;I should learn that list&#39;:</p>
+
+    <StyledLi>WebHooks</StyledLi>
+
+    <StyledLi>
+      GitHub integrations, I really want to build a badge-bot!
+    </StyledLi>
+
+    <h3>Other interests</h3>
+    <p>
+      I try to get to the gym three times a week to weight lift, that
+      took up quite a bit of my time over the last 3 years or so. I
+      have focused on the compound lifts bench, press, squat and my
+      favorite, deadlift!
+    </p>
+    <p>
+      Before that I used to play tennis three times a week up to a
+      county standard, I&#39;d love to have the time to play tennis
+      again but between all the above there&#39;s not much time left.
+      🙃
+    </p>
+
+    <p>
+      If there&#39;s anything you want to ask, you can Ask Me Anything
+    </p>
 
     <p>
       A little bit about me, I have been a VBA developer since 2007
@@ -65,7 +146,6 @@ const AboutPage = () => (
       with you if you would like to know more about me.
     </p>
     <p>All the best. 👍</p>
-
     <Link to="/">Go back to the homepage</Link>
   </PageWrapper>
 )
