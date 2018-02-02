f4a100cca56731654890a5cb9e157ff379f8a0d0
spences10 <spences10apps@gmail.com>
2018-02-02 17:43:01 +0000

    add some now detail

--- src/pages/now.js
+++ src/pages/now.js
@@ -6,8 +6,31 @@ import { PageWrapper } from '../theme/globalStyle'
 
 const NowPage = () => (
   <PageWrapper>
-    <p>now things</p>
-    <Link to="/">Go home</Link>
+    <h1>What I&#39;m Doing Now</h1>
+    <small>February 2018</small>
+    <h3>Work</h3>
+    <p>
+      Currently PM for a GitHub community project{' '}
+      <span>
+        {' '}
+        <code>grad.then()</code>
+      </span>{' '}
+      working with two other remote developers in Egypt and Serbia.{' '}
+      <span>
+        {' '}
+        <code>grad.then()</code>
+      </span>{' '}
+      is intended as a resource for post bootcamp graduates to use as
+      a resource for finding a full time role.
+    </p>
+    <h3>Play</h3>
+    <p>
+      Back to the gym after Christmas break, I&#39;m going to try
+      focus more on conditioning and cardio work for a while.
+    </p>
+    <p>
+      <Link to="/">Go home</Link>
+    </p>
   </PageWrapper>
 )
 
