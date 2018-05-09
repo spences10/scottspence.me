65e8f9a69bbb6b186b51b46689dd6534894c0b96
spences10 <spences10apps@gmail.com>
2018-05-09 06:32:26 +0100

    do a gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,45 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  const faqTemplate = path.resolve('src/templates/faqTemplate.js')
+
+  const aboutTemplate = path.resolve('src/templates/faqTemplate.js')
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: faqTemplate,
+        context: {} // additional data can be passed via context
+      })
+    })
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: aboutTemplate,
+        context: {} // additional data can be passed via context
+      })
+    })
+  })
+}
