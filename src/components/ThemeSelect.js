443104e158ca0c06abf878b43764e0504cd39dc4
spences10 <spences10apps@gmail.com>
2018-07-04 06:57:08 +0100

    import context and themes make ThemeSelect a sfc

--- src/components/ThemeSelect.js
+++ src/components/ThemeSelect.js
@@ -2,6 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
+import { BlogThemeContext } from '../contexts/BlogThemeContext'
+import { themes } from '../theme/globalStyle'
+
 const SelectWrapper = styled.div`
   grid-area: t;
   margin: 0rem 0.5rem 0rem 0.25rem;
@@ -24,17 +27,24 @@ export const SelectOpt = styled.option`
   /* font-size: 1rem; */
 `
 
-class ThemeSelect extends React.Component {
-  render() {
-    return (
-      <SelectWrapper>
-        <Select onChange={e => this.props.handleThemeChange(e)}>
-          <SelectOpt value="theme1">Theme 1</SelectOpt>
-          <SelectOpt value="theme2">Theme 2</SelectOpt>
-        </Select>
-      </SelectWrapper>
-    )
-  }
+const ThemeSelect = () => {
+  return (
+    <BlogThemeContext.Consumer>
+      {({ handleThemeChange }) => (
+        <SelectWrapper>
+          <Select onChange={e => handleThemeChange(e)}>
+            {Object.keys(themes).map((theme, index) => {
+              return (
+                <SelectOpt key={index} value={theme}>
+                  Theme {index + 1}
+                </SelectOpt>
+              )
+            })}
+          </Select>
+        </SelectWrapper>
+      )}
+    </BlogThemeContext.Consumer>
+  )
 }
 
 ThemeSelect.propTypes = {
