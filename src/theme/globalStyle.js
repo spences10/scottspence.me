f91f20a836fbe1ed05a8894e20535c5e838f9c0e
spences10 <spences10apps@gmail.com>
2018-09-11 06:58:24 +0100

    add global style

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,50 @@
+export const themes = {
+  theme1: {
+    primary: '#6e27c5',
+    primaryAccent: '#eb238e',
+    branding: '#f7e018',
+    secondary: '#fcdeb6',
+    background: '#f9f9fd',
+    foreground: '#fefefe',
+    border: '#044040',
+    fontHeader: 'Dosis, sans-serif',
+    fontBody: 'Poppins, sans, sans-serif',
+    fontDark: '#34434b',
+    fontLight: '#586368',
+    fontWhite: '#ffffff',
+    pink: '#ff0198',
+    blue: '#01c1d6'
+  },
+  theme2: {
+    primary: '#ff0198',
+    primaryAccent: '#eb238e',
+    branding: '#01c1d6',
+    secondary: '#ffb617',
+    background: '#e6e6e6',
+    foreground: '#f7f0f0',
+    border: '#054545',
+    fontHeader: 'Poppins, sans, sans-serif',
+    fontBody: 'Dosis, sans-serif',
+    fontDark: '#034544',
+    fontLight: '#596869',
+    fontWhite: '#ffffff',
+    pink: '#ff52ba',
+    blue: '#59eeff'
+  },
+  theme3: {
+    primary: '#1cbcd2',
+    primaryAccent: '#69479e',
+    branding: '#f16623',
+    secondary: '#ffb617',
+    background: '#f3eff5',
+    foreground: '#88d4d8',
+    border: '#055455',
+    fontHeader: 'Eczar, sans, sans-serif',
+    fontBody: 'Trirong, sans-serif',
+    fontDark: '#0d0a0b',
+    fontLight: '#454955',
+    fontWhite: 'fffff6',
+    pink: '#ff73c7',
+    blue: '#00e5ff'
+  }
+}
