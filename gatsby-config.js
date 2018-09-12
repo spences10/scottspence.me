b7e69b34b155ae5bf23fcb98cc25de47dbc8ea3c
spences10 <spences10apps@gmail.com>
2018-09-12 06:33:27 +0100

    add styled-components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -13,9 +13,10 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
     },
     'gatsby-plugin-offline',
-  ],
+    'gatsby-plugin-styled-components'
+  ]
 }
