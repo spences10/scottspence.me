cb59ce2188bebac968c95523fe05dc68ad747e4c
spences10 <spences10apps@gmail.com>
2018-09-12 06:22:45 +0100

    add site config

---
+++ data/siteConfig.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteTitle: 'scottspence.me',
+  siteUrl: 'https://v2scottspence.me',
+  nameContent: 'Scott Spence - blog',
+  developerName: 'Scott Spence',
+  developerUrl: 'https.scottspence.me',
+  descriptionContent: 'Scott Spence - web developer',
+  keywordsContent:
+    'web developer, javascript, react, portfolio, information',
+  imageLink: 'https://scottspence.me/icons/icon-512x512.png',
+  faviconPng: './src/images/favicon.png',
+  contact: {
+    github: 'https://github.com/spences10',
+    medium: 'https://medium.com/@spences10',
+    devto: 'https://dev.to/spences10',
+    twitter: 'https://twitter.com/ScottDevTweets',
+    email: 'spences10apps@gmail.com'
+  },
+  backgroundColour: '#663399', // this is for favicon and manifest
+  themeColour: '#755f9f', // this is for favicon and manifest
+  siteLanguage: 'en-GB'
+}
