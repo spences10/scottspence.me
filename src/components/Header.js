095f8b69ff1724b5c1afbc918b44e990bfb800ec
spences10 <spences10apps@gmail.com>
2018-09-14 06:40:59 +0100

    basic style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.header`
+  color: white;
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+  margin: 0 auto;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <HeaderWrapper>
+    <h1>
+      <Link to="/">{siteTitle}</Link>
+    </h1>
+  </HeaderWrapper>
 )
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,6 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import { GlobalStyle } from '../theme/globalStyle'
-import './layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -31,15 +30,7 @@ const Layout = ({ children }) => (
           <html lang="en-GB" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0
-          }}>
-          {children}
-        </div>
+        <div>{children}</div>
       </>
     )}
   />
