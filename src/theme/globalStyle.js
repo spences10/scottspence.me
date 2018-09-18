e82c0b86bc9d01bb7912bfc5f364418f985c72ff
spences10 <spences10apps@gmail.com>
2018-09-18 21:57:29 +0100

    unfuck the url

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -52,29 +52,21 @@ export const themes = {
 }
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('
-    https://fonts.googleapis.com/css?family=
-    Dosis:400,700|
-    Poppins:400,700|
-    Trirong:400,700|
-    Rubik:400,700|
-    Nunito:400,700|
-    Eczar:400,700|
-    Taviraj:400,700
-  ');
-
-  *, *:before, *:after {
-    box-sizing: border-box;
-  }
-
+  @import url('https://fonts.googleapis.com/css?family=Dosis:400,700|Poppins:400,700|Trirong:400,700|Rubik:400,700|Nunito:400,700|Eczar:400,700|Taviraj:400,700');
+  
   body {
     margin: 0;
     color: hsla(0, 0%, 0%, 0.8);
   }
 
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+
   a {
     text-decoration: none;
   }
+
 `
 
 const sizes = {
