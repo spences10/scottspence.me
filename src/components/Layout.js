bd5ef96749d1512c68a8cf7568ac0403b4da367c
spences10 <spences10apps@gmail.com>
2018-09-19 06:33:06 +0100

    use context provider fro theme

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 
 import Header from './Header'
-import { GlobalStyle } from '../theme/globalStyle'
+import {
+  ThemeSelectContext,
+  ThemeSelectProvider
+} from '../contexts/ThemeSelectContext'
 
+import { GlobalStyle, themes } from '../theme/globalStyle'
 import { media } from '../theme/globalStyle'
 
-const PageContainer = styled.div`
+const AppStyles = styled.div`
+  background-color: ${({ theme }) => theme.background};
+  background-image: url("${props => props.background}");
+  background-attachment: fixed;
+  /*space between first post and nav bar*/
+  /* padding-top: 2.7rem; */
   display: grid;
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: auto;
@@ -21,27 +29,27 @@ const PageContainer = styled.div`
     grid-template-areas:
       'h h h h h h h h h h h h'
       '. . m m m m m m m m . .'
-      '. . f f f f f f f f . .';
+      'f f f f f f f f f f f f';
     /* background: goldenrod; */
   `};
   ${media.desktop`
     grid-template-areas:
       'h h h h h h h h h h h h'
-      '. . m m m m m m m m . .'
-      '. . f f f f f f f f . .';
+      '. m m m m m m m m m m .'
+      'f f f f f f f f f f f f';
     /* background: dodgerblue; */
   `};
   ${media.tablet`
-  grid-template-columns: repeat(9, 1fr);
-  grid-template-areas:
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-areas:
       'h h h h h h h h h'
       '. m m m m m m m .'
       'f f f f f f f f f';
     /* background: mediumseagreen; */
   `};
   ${media.phone`
-  grid-template-columns: repeat(9, 1fr);
-  grid-template-areas:
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-areas:
       'h h h h h h h h h'
       'm m m m m m m m m'
       'f f f f f f f f f';
@@ -49,36 +57,32 @@ const PageContainer = styled.div`
   `};
 `
 
+const Wrapper = styled.div`
+  grid-area: m;
+`
+
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            pages
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <GlobalStyle />
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' }
-          ]}>
-          <html lang="en-GB" />
-        </Helmet>
-        <PageContainer>
-          <Header siteTitle={data.site.siteMetadata.title} />
-          <div>{children}</div>
-        </PageContainer>
-      </>
-    )}
-  />
+  <ThemeSelectProvider>
+    <ThemeSelectContext.Consumer>
+      {({ theme }) => (
+        <ThemeProvider theme={themes.theme1}>
+          <AppStyles>
+            <GlobalStyle />
+            <Helmet
+              title={'data.site.siteMetadata.title'}
+              meta={[
+                { name: 'description', content: 'Sample' },
+                { name: 'keywords', content: 'sample, something' }
+              ]}>
+              <html lang="en-GB" />
+            </Helmet>
+            <Header siteTitle={'data.site.siteMetadata.title'} />
+            <Wrapper>{children}</Wrapper>
+          </AppStyles>
+        </ThemeProvider>
+      )}
+    </ThemeSelectContext.Consumer>
+  </ThemeSelectProvider>
 )
 
 Layout.propTypes = {
