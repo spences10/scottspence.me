04b643c5f798521abf477d6b0e89bcabe661883e
spences10 <spences10apps@gmail.com>
2018-09-23 17:51:58 +0100

    portfolio and project card

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+
+import { ButtonSmall } from './Button'
+import { HEROS } from '../theme/themeConstants'
+
+const Name = styled.h3`
+  margin: 0.5rem;
+  padding: 0.5rem;
+  color: ${props => props.theme.fontWhite};
+  font-family: ${props => props.theme.fontHeader};
+  font-weight: 900;
+  text-align: center;
+`
+
+const Desc = styled.div`
+  margin: 0rem;
+  padding: 1rem 1.5rem 1rem 1.5rem;
+  border-radius: 0px 0px 5px 5px;
+  color: ${props => props.theme.fontDark};
+  background-color: ${props => props.theme.foreground};
+  text-align: center;
+  font-family: ${props => props.theme.fontBody};
+  font-weight: 700;
+  line-height: 150%;
+`
+
+const StyledLink = styled.a``
+
+const CardWrapper = styled.div`
+  margin: 0rem;
+  padding: 0rem;
+  border-radius: 5px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
+  &:hover {
+    transform: translateY(-3px);
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.8);
+  }
+  background-color: ${props => props.theme.foreground};
+`
+
+const Card = styled.div`
+  padding: 1.75rem;
+  margin: 0rem;
+  border-radius: 5px 5px 0px 0px;
+  color: ${props => props.theme.foreground};
+  background-color: ${props =>
+    props.id % 2 === 0
+      ? `${props.theme.pink}`
+      : `${props.theme.blue}`};
+  background-image: url("${HEROS.WIGGLE}");
+`
+
+const ImageWrapper = styled.div`
+  display: grid;
+  justify-items: center;
+`
+
+const CardButton = ButtonSmall.extend`
+  text-transform: none;
+  border: 2px solid ${props => props.theme.fontWhite};
+  color: ${props => props.theme.fontWhite};
+  font-family: ${props => props.theme.fontBody};
+  font-weight: 700;
+  margin: 0.5rem 0.6rem 0.5rem 0.6rem;
+  padding: 0.5rem 0.8rem 0.5rem 0.8rem;
+  border-radius: 50px;
+`
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const ProjectCard = props => {
+  // childImageSharp.resolutions as cis
+  const { resolutions: cis } = props.imgData.node.childImageSharp
+  // console.log('=====props==========')
+  // console.log(props.imgData.node.childImageSharp.resolutions)
+  // console.log(cis)
+  // console.log('====================')
+  return (
+    <CardWrapper key={props.id} id={props.id}>
+      <Card id={props.id}>
+        <ImageWrapper>
+          <Img
+            style={{
+              borderRadius: '100%',
+              border: '2px solid white'
+            }}
+            key={props.id}
+            alt={`${props.name} image`}
+            resolutions={cis}
+          />
+        </ImageWrapper>
+        <Name>{props.name}</Name>
+        <ButtonsWrapper>
+          <StyledLink
+            target="_blank"
+            rel="noopener"
+            href={props.demo}>
+            <CardButton>Demo</CardButton>
+          </StyledLink>
+          <StyledLink
+            target="_blank"
+            rel="noopener"
+            href={props.github}>
+            <CardButton>GitHub</CardButton>
+          </StyledLink>
+        </ButtonsWrapper>
+      </Card>
+      <Desc>{props.desc}</Desc>
+    </CardWrapper>
+  )
+}
+
+ProjectCard.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  desc: PropTypes.string,
+  github: PropTypes.string,
+  demo: PropTypes.string,
+  image: PropTypes.string,
+  imgData: PropTypes.object
+}
+
+export default ProjectCard
---
+++ src/pages/portfolio.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+// import { ProjectCard } from '../components/ProjectCard'
+import styled from 'styled-components'
+// import Img from 'gatsby-image'
+
+import { media } from '../theme/globalStyle'
+import Layout from '../components/Layout'
+
+// import { projectsApi } from '../utils/api'
+
+// import { PageWrapper, ShinyButton } from '../theme/globalStyle'
+
+const ProjectWrapper = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(3, 1fr);
+  ${media.giant`
+    grid-template-columns: repeat(2, 1fr);
+  `};
+  ${media.desktop`
+    grid-template-columns: repeat(2, 1fr);
+  `};
+  ${media.tablet`
+    grid-template-columns: repeat(1, 1fr);
+  `};
+  ${media.phone`
+    grid-template-columns: repeat(1, 1fr);
+  `};
+`
+
+const PortfolioPage = () => {
+  // const { edges: imgData } = data.allFile
+  // console.log('====================')
+  // console.log(data)
+  // console.log(imgData)
+  // console.log('====================')
+
+  // projectsApi.projects.map(project => {
+  //   console.log('=====projectsApi==========')
+  //   console.log(project.image)
+  //   console.log('====================')
+  // })
+
+  // imgData.map(property => {
+  //   console.log('=====imgData==========')
+  //   console.log(property.node.relativePath)
+  //   console.log('====================')
+  // })
+
+  return (
+    <Layout>
+      <h1>Portfolio</h1>
+      <p>List of projects here:</p>
+      {/* {imgData.map((property, index) => (
+        <Img
+          key={index}
+          alt={property.node.relativePath}
+          resolutions={property.node.childImageSharp.resolutions}
+        />
+      ))} */}
+
+      <ProjectWrapper>
+        {/* {imgData.map(property => {
+          property.node.relativePath
+          property.node.childImageSharp.resolutions
+        })}
+        {projectsApi.projects.map(project => (
+          <Project key={project.id} {...project} />
+        ))} */}
+
+        {/* {projectsApi.projects.map((project, index) => {
+          project.imgData = imgData[index]
+          return <ProjectCard key={project.id} {...project} />
+        })} */}
+      </ProjectWrapper>
+
+      <p>
+        <Link to="/">
+          {/* <ShinyButton>homepage</ShinyButton> */}
+        </Link>
+      </p>
+    </Layout>
+  )
+}
+
+// export const query = graphql`
+//   query GatsbyImageQuery {
+//     allFile(
+//       sort: { order: ASC, fields: [absolutePath] }
+//       filter: { relativePath: { regex: "/project/" } }
+//     ) {
+//       edges {
+//         node {
+//           relativePath
+//           childImageSharp {
+//             resolutions(width: 200, height: 200) {
+//               ...GatsbyImageSharpResolutions
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+
+PortfolioPage.propTypes = {
+  data: PropTypes.object.isRequired
+}
+
+export default PortfolioPage
