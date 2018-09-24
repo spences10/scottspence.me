74413cc7a73b219a76ae0ef7bf1497ff753238af
spences10 <spences10apps@gmail.com>
2018-09-24 05:58:22 +0100

    add md pages

---
+++ src/templates/mdTemplate.js
@@ -0,0 +1,57 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+
+// import { PageWrapper, ShinyButton } from '../theme/globalStyle'
+import Layout from '../components/Layout'
+
+const MdTitle = styled.h1``
+
+const MdDate = styled.span`
+  font-weight: 700;
+  font-size: 0.8rem;
+  color: ${props => props.theme.fontLight};
+`
+
+const Template = ({
+  data // this prop will be injected by the GraphQL query below.
+}) => {
+  const { markdownRemark } = data // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <Layout>
+      <div className="md-post">
+        <MdTitle>{frontmatter.title}</MdTitle>
+        <MdDate>Updated: {frontmatter.date}</MdDate>
+        <div
+          className="md-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+      <Link to="/">{/* <ShinyButton>homepage</ShinyButton> */}</Link>
+    </Layout>
+  )
+}
+
+// Template.propTypes = {
+//   data: PropTypes.object
+// }
+
+export default Template
+
+/* eslint-disable */
+export const pageQuery = graphql`
+  query MDPageByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "YYYY MMMM Do")
+        path
+        title
+      }
+    }
+  }
+`
+/* eslint-enable */
