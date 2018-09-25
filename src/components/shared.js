51c32ed8c0a75c6f9fbfbece99f609a43f0dc135
spences10 <spences10apps@gmail.com>
2018-09-25 06:54:13 +0100

    add small butts

--- src/components/shared.js
+++ src/components/shared.js
@@ -52,3 +52,29 @@ export const StyledHyperLink = styled.a`
   }
   color: ${props => props.theme.primary};
 `
+
+export const Button = styled.button`
+  color: ${props => props.color};
+  font-size: 1rem;
+  text-transform: uppercase;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  border: 1px solid ${props => props.border};
+  background-color: Transparent;
+  border-radius: 4px;
+  transition: all 0.1s;
+  &:hover {
+    transform: translateY(1px);
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  }
+`
+
+export const ButtonSmall = styled(Button)`
+  padding: 0.1rem 2rem;
+  margin: 0.2rem;
+`
+
+export const ButtonBig = styled(Button)`
+  padding: 0.75rem 2rem;
+  margin: 0.5rem;
+`
