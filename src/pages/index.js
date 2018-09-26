937d4566198590f067406154c4c1d2c3e7459e7e
spences10 <spences10apps@gmail.com>
2018-09-26 06:37:06 +0100

    import banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 // import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-
-import { Dump } from '../util/helpers'
+import Banner from '../components/banner'
+// import { Dump } from '../util/helpers'
 
 const IndexPage = data => (
   <Layout>
+    <Banner />
     <h1>Hi people</h1>
-    <Dump data={data} />
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    {/* <Dump data={data} /> */}
+    <p>Welcome to my personal portfolio site.</p>
+    <p>Built with Gatsby and Styled Components 💅</p>{' '}
     {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
