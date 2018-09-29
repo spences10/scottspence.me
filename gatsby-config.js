60b72dac1f23f7fa104c1a6738cb00263f989628
Scott Spence <spences10apps@gmail.com>
2018-09-29 08:33:32 +0100

    Update manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: config.siteTitle,
+        short_name: config.siteTitle,
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: config.backgroundColour,
+        theme_color: config.themeColour,
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: config.faviconPng // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-offline',
