117391d7b497d6774a2bf83fbd0afacaa3901b54
spences10 <spences10apps@gmail.com>
2018-09-30 15:30:27 +0100

    template literals

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ function encode(data) {
   return Object.keys(data)
     .map(
       key =>
-        encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+        `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
     )
     .join('&')
 }
