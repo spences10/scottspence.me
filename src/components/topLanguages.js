98854524c0d1143a07ff79c202ad754c349cac64
spences10 <spences10apps@gmail.com>
2018-10-29 05:48:07 +0000

    add in responsive PIE!

--- src/components/topLanguages.js
+++ src/components/topLanguages.js
@@ -2,15 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql, StaticQuery } from 'gatsby'
 import { ResponsivePie } from 'nivo'
-import { topLanguages } from '../services/dataMassagers'
+import { topLanguages } from '../services/dataMassage'
+// import { Dump } from '../util/helpers'
 
-const TopLanguages = props => {
+const TopLanguages = ({ data }) => {
+  const { repositories } = data.github.viewer
   return (
-    <React.Fragment>
-      <p>jsx goes here</p>
-    </React.Fragment>
+    <ResponsivePie
+      data={topLanguages(repositories)}
+      margin={{
+        top: 40,
+        right: 80,
+        bottom: 80,
+        left: 80
+      }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+    />
   )
 }
+// <Dump data={data.github.viewer.repositories} />
 
 TopLanguages.propTypes = {}
 
