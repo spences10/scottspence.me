5555ae047452cf8f0baefc835679ffe15f84fa88
spences10 <spences10apps@gmail.com>
2018-10-29 06:29:55 +0000

    add wrapper

--- src/components/topLanguages.js
+++ src/components/topLanguages.js
@@ -3,23 +3,35 @@ import PropTypes from 'prop-types'
 import { graphql, StaticQuery } from 'gatsby'
 import { ResponsivePie } from 'nivo'
 import { topLanguages } from '../services/dataMassage'
+import styled from 'styled-components'
 // import { Dump } from '../util/helpers'
+
+const LanguageWrapper = styled.div`
+  width: 100%;
+  height: 55vh;
+  max-height: 700px;
+  max-width: 700px;
+  margin: 0 auto;
+  padding: 40;
+`
 
 const TopLanguages = ({ data }) => {
   const { repositories } = data.github.viewer
   return (
-    <ResponsivePie
-      data={topLanguages(repositories)}
-      margin={{
-        top: 40,
-        right: 80,
-        bottom: 80,
-        left: 80
-      }}
-      innerRadius={0.5}
-      padAngle={0.7}
-      cornerRadius={3}
-    />
+    <LanguageWrapper>
+      <ResponsivePie
+        data={topLanguages(repositories)}
+        margin={{
+          top: 40,
+          right: 80,
+          bottom: 80,
+          left: 80
+        }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+      />
+    </LanguageWrapper>
   )
 }
 // <Dump data={data.github.viewer.repositories} />
