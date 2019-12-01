98caf5936733d0debe6de0f774b043f9640991ba
spences10 <spences10apps@gmail.com>
2019-12-01 18:10:39 +0000

    fold in components

--- src/root-wrapper.js
+++ src/root-wrapper.js
@@ -1,21 +1,18 @@
 import { MDXProvider } from '@mdx-js/react';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-// import { H1, H2, P } from './src/components/pageElements';
-import { theme } from './theme/globalStyle';
+import { H1, H2, H3, P } from './components/page-elements';
+import { theme } from './theme/global-style';
 
-// const components = {
-//   h1: props => <H1 {...props} />,
-//   h2: props => <H2 {...props} />,
-//   p: props => <P {...props} />,
-// };
+const components = {
+  h1: props => <H1 {...props} />,
+  h2: props => <H2 {...props} />,
+  h3: props => <H3 {...props} />,
+  p: props => <P {...props} />,
+};
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>
-    <MDXProvider
-    // components={components}
-    >
-      {element}
-    </MDXProvider>
+    <MDXProvider components={components}>{element}</MDXProvider>
   </ThemeProvider>
 );
