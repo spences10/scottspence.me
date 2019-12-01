5352183f289604ceb797790d440a6270d61a554a
spences10 <spences10apps@gmail.com>
2019-12-01 10:07:55 +0000

    add base globalStyle

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,48 +1,65 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 export const theme = {
-  primary: '#6e27c5',
-  primaryAccent: '#eb238e',
-  branding: '#f7e018',
-  secondary: '#fcdeb6',
-  background: '#f9f9fd',
-  foreground: '#fefefe',
-  border: '#044040',
-  fontHeader: 'Dosis, sans-serif',
-  fontBody: 'Poppins, sans, sans-serif',
-  fontDark: '#34434b',
-  fontLight: '#586368',
-  fontWhite: '#ffffff',
-  pink: '#ff0198',
-  blue: '#01c1d6'
-}
+  colours: {
+    primary: {
+      100: '#663399',
+    },
+    dark: {
+      100: '#1e151f',
+      200: '#463c47',
+      300: '#726773',
+    },
+    light: {
+      100: '#e5d8e6',
+      200: '#d4c7d5',
+      300: '#a79ba8',
+    },
+  },
+  fonts: {
+    header: 'Dosis, sans-serif',
+    body: 'Poppins, sans, sans-serif',
+  },
+  fontSizes: {
+    h1: '40px',
+    h2: '26px',
+    h3: '24px',
+    p: '21px',
+  },
+};
 
 const fonts = () => {
-  const list = [
-    'Dosis:400,700|',
-    'Poppins:400,700|',
-    'Trirong:400,700|',
-    'Rubik:400,700|',
-    'Nunito:400,700|',
-    'Eczar:400,700|',
-    'Taviraj:400,700|'
-  ]
-  return list.join('')
-}
+  const list = ['Dosis:400,700|', 'Poppins:400,700|'];
+  return list.join('');
+};
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=${fonts}');
+
+  ${reset}
   
-  body {
-    margin: 0;
-    color: hsla(0, 0%, 0%, 0.8);
-  }
+  /* use for debugging only!! */
+  /* * {
+    outline: 1px solid red !important;
+  } */
 
   *, *:before, *:after {
     box-sizing: border-box;
   }
-
-  a {
-    text-decoration: none;
+  html {
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    font-family: ${({ theme }) => theme.fonts.body};
+    color: ${({ theme }) => theme.colours.dark[100]};
   }
-`
+  body {
+    font-size: 16px;
+    font-weight: 500;
+    padding: 0;
+    margin: 0;
+    line-height: 1.9;
+    overflow-x: initial;
+    background-color: ${({ theme }) => theme.primary}
+  }
+`;
