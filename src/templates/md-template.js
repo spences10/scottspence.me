071ad4a109f3a3553799e03f68872cd8791e454d
spences10 <spences10apps@gmail.com>
2019-12-06 08:39:45 +0000

    use toc for title

--- src/templates/md-template.js
+++ src/templates/md-template.js
@@ -3,25 +3,15 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 import PropTypes from 'prop-types';
 import React from 'react';
 import SEO from 'react-seo-component';
-import styled from 'styled-components';
 import Layout from '../components/layout';
-import { H1 } from '../components/page-elements';
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
-
-const MdTitle = styled(H1)``;
-
-const MdDate = styled.span`
-  font-weight: 700;
-  font-size: 0.8rem;
-  color: ${props => props.theme.fontLight};
-`;
 
 const Template = ({ data }) => {
   const {
-    frontmatter: { title, date },
     body,
     excerpt,
     fields: { slug },
+    tableOfContents: { items },
   } = data.mdx;
   const {
     imageLink,
@@ -34,20 +24,18 @@ const Template = ({ data }) => {
   return (
     <Layout>
       <SEO
-        title={title}
+        title={items[0].title}
         description={excerpt}
         image={`${siteUrl}${imageLink}`}
         pathname={`${siteUrl}${slug}`}
         publishedDate={'2019-12-01T08:26:52.963Z'}
-        modifiedDate={date}
+        modifiedDate={new Date(Date.now()).toISOString()}
         siteLanguage={siteLanguage}
         siteLocale={siteLocale}
         twitterUsername={twitterUsername}
         author={developerName}
         article={true}
       />
-      <MdTitle>{title}</MdTitle>
-      <MdDate>Updated: {date}</MdDate>
       <MDXRenderer>{body}</MDXRenderer>
     </Layout>
   );
@@ -62,15 +50,12 @@ Template.propTypes = {
 export const query = graphql`
   query PageBySlug($slug: String!) {
     mdx(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
-      }
       body
       excerpt
       fields {
         slug
       }
+      tableOfContents
     }
   }
 `;
