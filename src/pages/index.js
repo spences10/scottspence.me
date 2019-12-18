8656f199bf3cede9786e2e123f192e7d7d5c3d11
spences10 <spences10apps@gmail.com>
2019-12-18 05:55:19 +0000

    add device detect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { isIE } from 'react-device-detect';
 import Helmet from 'react-helmet';
 import SEO from 'react-seo-component';
 import styled from 'styled-components';
@@ -29,6 +30,13 @@ export default () => {
     siteLocale,
     twitterUsername,
   } = useSiteMetadata();
+  if (isIE)
+    return (
+      <h1>
+        IE is not supported. Please use a modern browser like Firefox
+        or Chrome.
+      </h1>
+    );
   return (
     <>
       <AnalyticsProvider>
