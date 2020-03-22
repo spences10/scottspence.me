cc0cf4227ff4aeea6b3df7b8c494fdf9551b6857
spences10 <spences10apps@gmail.com>
2020-03-22 11:38:18 +0000

    stacked card design

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -3,38 +3,54 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 
-const CardWrapper = styled.div`
-  background-color: ${({ theme }) => theme.colours.white};
-  border-radius: ${({ theme }) => theme.borderRadius.lg};
-  overflow: hidden;
-  border: solid 1px ${({ theme }) => theme.colours.grey[300]};
+const CardWrapper = styled.div``;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  padding-bottom: 83.333333%;
   img {
-    height: ${({ theme }) => theme.spacing[64]};
+    position: absolute;
+    height: 100%;
     width: 100%;
     object-fit: cover;
     object-position: top center;
+    border-radius: ${({ theme }) => theme.borderRadius.lg};
+    box-shadow: ${({ theme }) => theme.boxShadow.md};
+    filter: brightness(80%) saturate(60%);
+    &:hover {
+      filter: brightness(100%) saturate(100%);
+    }
   }
 `;
 
 const Card = styled.div`
-  padding: ${({ theme }) => theme.spacing[6]};
-  h4 {
-    font-weight: 600;
-    font-size: ${({ theme }) => theme.fontSize.lg};
-    margin-bottom: ${({ theme }) => theme.spacing[2]};
-  }
-  a {
-    font-weight: 700;
-    font-size: 0.65rem;
-    text-transform: uppercase;
-    color: ${({ theme }) => theme.colours.grey[400]};
-    letter-spacing: ${({ theme }) => theme.letterSpacing.wide};
-    padding-right: ${({ theme }) => theme.spacing[2]};
-  }
-  p {
-    color: ${({ theme }) => theme.colours.grey[700]};
-    line-height: ${({ theme }) => theme.lineHeight.snug};
-    font-size: ${({ theme }) => theme.fontSize.sm};
+  position: relative;
+  padding: ${({ theme }) => theme.spacing[4]};
+  margin-top: -${({ theme }) => theme.spacing[16]};
+  div {
+    background-color: ${({ theme }) => theme.colours.white};
+    padding: ${({ theme }) => theme.spacing[6]};
+    border-radius: ${({ theme }) => theme.borderRadius.lg};
+    box-shadow: ${({ theme }) => theme.boxShadow.lg};
+    h4 {
+      font-weight: 600;
+      font-size: ${({ theme }) => theme.fontSize.lg};
+      margin-bottom: ${({ theme }) => theme.spacing[2]};
+    }
+    a {
+      font-weight: 700;
+      font-size: 0.65rem;
+      text-transform: uppercase;
+      color: ${({ theme }) => theme.colours.grey[500]};
+      letter-spacing: ${({ theme }) => theme.letterSpacing.wide};
+      padding-right: ${({ theme }) => theme.spacing[2]};
+    }
+    p {
+      color: ${({ theme }) => theme.colours.grey[700]};
+      line-height: ${({ theme }) => theme.lineHeight.snug};
+      font-size: ${({ theme }) => theme.fontSize.sm};
+      max-height: ${({ theme }) => theme.spacing[16]};
+    }
   }
 `;
 
@@ -48,12 +64,18 @@ export const ProjectCard = ({
 }) => {
   return (
     <CardWrapper>
-      <img src={image} alt={name} />
+      <ImageWrapper>
+        <img src={image} alt={name} />
+      </ImageWrapper>
       <Card>
-        <a href={github}>GitHub</a>
-        <a href={demo}>Demo</a>
-        <h4>{name}</h4>
-        <p>{desc}</p>
+        <div>
+          <a href={github}>GitHub</a>
+          <a href={demo}>Demo</a>
+          <h4>{name}</h4>
+          <p>
+            {desc.length > 65 ? `${desc.substring(0, 65)}...` : desc}
+          </p>
+        </div>
       </Card>
     </CardWrapper>
   );
