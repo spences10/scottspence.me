8022716fcdd289b3fd12ed5f0aa2851c5a609dd7
spences10 <spences10apps@gmail.com>
2020-03-23 07:22:21 +0000

    add breakpoints for responsive

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { up } from 'styled-breakpoints';
 import styled from 'styled-components';
 import { useGraphCMSData } from '../hooks/use-graphcms-data';
 import { ProjectCard } from './project-card';
 
 const ProjectWrapper = styled.div`
   display: grid;
-  grid-gap: 20px;
-  grid-template-columns: repeat(3, 1fr);
-  margin: 0 -50%;
+  grid-gap: ${({ theme }) => theme.spacing[5]};
+  ${up('sm')} {
+    /* background-color: rebeccapurple; */
+    grid-template-columns: repeat(1, 1fr);
+    margin: 0 auto;
+  }
+  ${up('md')} {
+    /* background-color: mediumseagreen; */
+  }
+  ${up('lg')} {
+    /* background-color: burlywood; */
+    grid-template-columns: repeat(2, 1fr);
+    margin: 0 -20%;
+  }
+  ${up('xl')} {
+    /* background-color: blueviolet; */
+    grid-template-columns: repeat(3, 1fr);
+    margin: 0 -50%;
+  }
 `;
 
 export const Projects = () => {
