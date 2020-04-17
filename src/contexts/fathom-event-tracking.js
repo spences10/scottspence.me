27c7fff1a9c4690dc95dad4c7ed682946ac99426
spences10 <spences10apps@gmail.com>
2020-04-17 18:16:13 +0100

    move analytics provider to root wrapper

--- src/contexts/fathom-event-tracking.js
+++ src/contexts/fathom-event-tracking.js
@@ -1,28 +1,31 @@
-import React, { createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext, useEffect } from 'react'
 
-const AnalyticsContext = createContext({});
+const AnalyticsContext = createContext({})
 
 export const AnalyticsProvider = ({ children }) => {
   useEffect(() => {
     if (typeof window.fathom === 'undefined') {
       window.fathom = (x, y, z) => {
-        console.log(`I'm a fake Fathom`, x, y, z);
-      };
+        console.log(`I'm a fake Fathom`, x, y, z)
+      }
     }
-  }, []);
+  }, [])
 
   const events = {
     logNavigation: goalId => {
-      window.fathom('trackGoal', goalId, 0);
+      window.fathom('trackGoal', goalId, 0)
     },
-  };
+    logLinkClick: goalId => {
+      window.fathom('trackGoal', goalId, 0)
+    },
+  }
   return (
     <AnalyticsContext.Provider value={events}>
       {children}
     </AnalyticsContext.Provider>
-  );
-};
+  )
+}
 
 export const useAnalytics = () => {
-  return useContext(AnalyticsContext);
-};
+  return useContext(AnalyticsContext)
+}
