9c36febdc1c5a9fc3c79f0d704d3ad53128b538d
spences10 <spences10apps@gmail.com>
2020-04-19 13:33:45 +0100

    Prettier

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
-const { createFilePath } = require(`gatsby-source-filesystem`);
-const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
 
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
-  const mdTemplate = path.resolve('src/templates/md-template.js');
+  const { createPage } = actions
+  const mdTemplate = path.resolve('src/templates/md-template.js')
 
   return graphql(`
     {
@@ -19,10 +19,10 @@ exports.createPages = ({ actions, graphql }) => {
     }
   `).then(result => {
     if (result.errors) {
-      throw result.errors;
+      throw result.errors
     }
 
-    const pages = result.data.allMdx.nodes;
+    const pages = result.data.allMdx.nodes
 
     pages.forEach(page => {
       createPage({
@@ -31,19 +31,19 @@ exports.createPages = ({ actions, graphql }) => {
         context: {
           slug: page.fields.slug,
         },
-      });
-    });
-  });
-};
+      })
+    })
+  })
+}
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
+  const { createNodeField } = actions
   if (node.internal.type === `Mdx`) {
-    const value = createFilePath({ node, getNode });
+    const value = createFilePath({ node, getNode })
     createNodeField({
       name: `slug`,
       node,
       value,
-    });
+    })
   }
-};
+}
