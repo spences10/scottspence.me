c9b6f18836600ff044df07bd4fbab5310ca5393c
spences10 <spences10apps@gmail.com>
2020-05-05 16:45:14 +0100

    helmet import

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,10 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import Helmet from 'react-helmet';
-import styled from 'styled-components';
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import styled from 'styled-components'
 // import Dump from '@wesbos/dump'
-import { GlobalStyle } from '../theme/global-style';
-import { media } from '../theme/sizes';
+import { GlobalStyle } from '../theme/global-style'
+import { media } from '../theme/sizes'
 
 const AppStyles = styled.div`
   position: relative;
@@ -14,27 +14,27 @@ const AppStyles = styled.div`
   ${media.desktop`
     max-width: 70vw;
   `};
-`;
+`
 
 const Wrapper = styled.div`
   max-width: ${({ theme }) => theme.maxWidth};
   margin: 0 auto;
-`;
+`
 
 const Layout = ({ children }) => {
   return (
     <AppStyles>
       <GlobalStyle />
       <Helmet>
-        <html lang="en-GB" />
+        <html lang='en-GB' />
       </Helmet>
       <Wrapper>{children}</Wrapper>
     </AppStyles>
-  );
-};
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-};
+}
 
-export default Layout;
+export default Layout
